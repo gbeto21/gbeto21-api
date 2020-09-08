4a1f1b83fef0f62315874f389716c2210b16c98b
Alberto González Balderas <albertoglezb@hotmail.com>
2020-09-08 11:41:05 -0500

    Initial commit. The base structure of a get request was written.

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const graphqlHttp = require('express-graphql')
+const { buildSchema } = require('graphql')
+
+const app = express()
+
+app.use(bodyParser.json())
+app.use('/graphql',
+    graphqlHttp(
+        {
+            schema: buildSchema(``),
+            rootValue: {}
+        }
+    )
+)
+
+app.listen(3000)
