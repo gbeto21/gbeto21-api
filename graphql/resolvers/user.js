814b7bb21d42a1a2ce2b92d47da01d1fdf38221e
Alberto González Balderas <albertoglezb@hotmail.com>
2020-09-10 17:17:41 -0500

    The login funtionality was created.

--- graphql/resolvers/user.js
+++ graphql/resolvers/user.js
@@ -1,6 +1,7 @@
 const User = require('../../models/user')
 const HttpError = require('../../models/httperror')
 const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
 
 const getUser = args => {
 
@@ -29,6 +30,33 @@ module.exports = {
         } catch (error) {
             console.log(error);
             return new HttpError('Something went wrong, could not create the user.', 500)
+        }
+    },
+    login: async ({ email, password }) => {
+        try {
+            let user = await User.findOne({ email: email })
+            if (!user) {
+                return new HttpError('Invalid credentials.', 403)
+            }
+            let passwordsMatch = await bcrypt.compare(password, user.password)
+            if (!passwordsMatch) {
+                return new HttpError('Invalid credentials.', 403)
+            }
+
+            let token = jwt.sign(
+                {
+                    userId: user._id,
+                    email: user.email
+                },
+                `${process.env.SECRET_WORD}`,
+                { expiresIn: '1h' }
+            )
+
+            return { userId: user.id, token: token, tokenExpiration: 1 }
+
+        } catch (error) {
+            console.log(error);
+            return new HttpError('Something went wrong, could not login the user.', 500)
         }
     }
 }
